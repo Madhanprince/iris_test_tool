739936a28c312b53e8c0a79370b300e49c6600e7
maddy <93968056+Madhanprince@users.noreply.github.com>
2026-03-30 17:29:47 +0530

    Update main.cpp

--- src/test_tool/src/main.cpp
+++ src/test_tool/src/main.cpp
@@ -2,28 +2,11 @@
 
 #include <QApplication>
 
-#include <thread>
-
 int main(int argc, char *argv[])
 {
-    rclcpp::init(argc, argv);
     QApplication a(argc, argv);
-
-    auto node = std::make_shared<rclcpp::Node>("qt_ui_node");
-
-    // ROS spinning in separate thread
-    std::thread ros_thread([&]() {
-        rclcpp::spin(node);
-    });
-    MainWindow w(node);
-    // w.show();
-    w.showMaximized();
-
-    int result = a.exec();
-
-    rclcpp::shutdown();
-    ros_thread.join();
-    
-
-    return result;
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
+
