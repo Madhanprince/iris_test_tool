7fa014406ad2288d845d3059626556967a1e9093
maddy <madjeeve123@gmail.com>
2026-03-30 23:22:13 +0530

    UI

--- src/test_tool/src/mainwindow.cpp
+++ src/test_tool/src/mainwindow.cpp
@@ -1,15 +1,26 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // 🔷 Sidebar → Page Switching
+    connect(ui->listWidget, &QListWidget::currentRowChanged,
+            this, [=](int row){
+        if(row >= 0)
+            ui->stackedWidget->setCurrentIndex(row);
+    });
+
+    // 🔷 Default page
+    ui->listWidget->setCurrentRow(0);
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
